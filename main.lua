39c89e7aa47d44e40afa910a324dee3384e8ff40
suhawalid-coder <suha_walid@uomustansiriyah.edu.iq>
2026-04-02 10:05:52 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,9 @@
+print("🔥 Ahmed Hub Loaded")
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "احمد بطل",
+    Text = "اشتغل السكربت ✅",
+    Duration = 5
+})
+
+-- هنا حط سكربتك الكامل (Aimbot + ESP + Key)
